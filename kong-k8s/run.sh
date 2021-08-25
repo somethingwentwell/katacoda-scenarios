298774b3b10fe30aea8049edb2826434936db204
Warren Wong <wwwc@outlook.com>
2021-08-25 19:30:49 +0800

    update sh

--- kong-k8s/run.sh
+++ kong-k8s/run.sh
@@ -1,9 +1,8 @@
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+#!/bin/bash
 
-sudo apt-get install apt-transport-https --yes
-
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-
-sudo apt-get update
-
-sudo apt-get install helm
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add - \
+&& sudo apt-get install apt-transport-https --yes \
+&& echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list \
+&& sudo apt-get update \
+&& sudo apt-get install helm \
+&& helm version
